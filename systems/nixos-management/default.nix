ed6b1e66cf36d40b1085f391faf2c0c0472a6c56
Dmytro Kyrychuk <dmytro@kyrych.uk>
2022-07-03 15:55:43 +0300

    Cleanup nixos-management

--- systems/nixos-management/default.nix
+++ systems/nixos-management/default.nix
@@ -3,11 +3,7 @@
   imports = [
     (import "${inputs.self}/systems/proxmox-vm" {
       hostname = "nixos-management";
-      ip = "10.7.89.150";
       inherit inputs;
-    })
-    (import "${inputs.self}/modules/restic-server-client" {
-      time = "21:30"; inherit config custom inputs pkgs;
     })
     "${inputs.self}/modules/code-server"
     "${inputs.self}/modules/docker"
