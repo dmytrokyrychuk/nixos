9393397b0cd6785cca30533a9375575433a7f9ea
Dmytro Kyrychuk <dmytro@kyrych.uk>
2022-07-03 17:34:53 +0300

    Remove static ip configuration from proxmox-vm

--- systems/proxmox-vm/default.nix
+++ systems/proxmox-vm/default.nix
@@ -1,26 +1,9 @@
-{ inputs, hostname, ip, ... }:
+{ inputs, hostname, ... }:
 {
   imports = [
     "${inputs.self}/modules/ntp"
     "${inputs.self}/modules/syslog"
   ];
-
-  networking = {
-    useDHCP = false;
-    hostName = hostname;
-    hosts = {
-      "127.0.0.1" = [ "${hostname}.2li.local" ];
-      ip = [ "${hostname}.2li.local" ];
-    };
-    defaultGateway = "10.7.89.1";
-    nameservers = [ "10.7.89.2" ];
-    interfaces.ens18.ipv4.addresses = [
-      {
-        address = ip;
-        prefixLength = 24;
-      }
-    ];
-  };
 
   boot.initrd.availableKernelModules = [
     "ata_piix"
