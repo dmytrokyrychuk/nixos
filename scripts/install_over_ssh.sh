d6398b063d44023e9f042f5fd54a37f980ba9ded
Dmytro Kyrychuk <dmytro@kyrych.uk>
2022-07-03 15:49:34 +0300

    Rename install scripts

--- scripts/install_over_ssh.sh
+++ scripts/install_over_ssh.sh
@@ -3,9 +3,9 @@
 read -e -p "Enter a config you want to deploy: " flake
 
 server="nixos@nixos.2li.local"
-download_command="curl https://git.2li.ch/Nebucatnetzer/nixos/archive/master.tar.gz | tar xz"
+download_command="curl https://github.com/dmytrokyrychuk/nixos/archive/master.tar.gz | tar xz"
 install_command="cd ~/nixos && ./scripts/install_vm.sh $flake"
-rsa_key="~/.ssh/id_rsa"
+rsa_key="~/.ssh/id_ed25519"
 
 ssh-copy-id $server
 ssh -i $rsa_key -t $server $download_command
